6c49b59a2bd87cd82cdcf329e748990d3a423fe9
JosephPark <josephsssss@hotmail.com>
2022-07-14 16:34:01 +0900

    finalCommit

---
+++ typeconverter/src/test/java/hello/typeconverter/TypeconverterApplicationTests.java
@@ -0,0 +1,16 @@
+package hello.typeconverter;
+
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.type.IpPort;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class TypeconverterApplicationTests {
+
+
+
+}
---
+++ typeconverter/src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,27 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService(){
+        DefaultConversionService defaultConversionService = new DefaultConversionService();
+        defaultConversionService.addConverter(new StringToIntegerConverter());
+        defaultConversionService.addConverter(new IntegerToStringConverter());
+        defaultConversionService.addConverter(new StringToIpPortConverter());
+        defaultConversionService.addConverter(new IpPortToStringConverter());
+
+        assertThat(defaultConversionService.convert("10", Integer.class)).isEqualTo(10);
+        assertThat(defaultConversionService.convert(10, String.class)).isEqualTo("10");
+        IpPort ipPort = defaultConversionService.convert("127.0.0.1:8080", IpPort.class);
+        assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+        String ipPortString = defaultConversionService.convert(new IpPort("127.0.0.1", 8080), String.class);
+        assertThat(ipPortString).isEqualTo("127.0.0.1:8080");
+    }
+}
